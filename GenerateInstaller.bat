6e63dc2b806f7cb9c867a22886c839ae31835d24
Robert Walker <rrah99@gmail.com>
2018-04-06 23:58:28 +0100

    Use path for NSIS

--- GenerateInstaller.bat
+++ GenerateInstaller.bat
@@ -1,10 +1,10 @@
 rmdir dist /Q /S
 del PiVTDesktopSetup.exe
-C:\Python27\Scripts\pyinstaller.exe PiVTDesktop.py --noconsole -i PiVT_icon.ico --onefile
+C:\Python27\scripts\pyinstaller.exe PiVTDesktop.py --noconsole -i PiVT_icon.ico --onefile
 copy README.md dist
 copy COPYING.txt dist
 copy config.yaml dist
 copy PiVT_icon.ico dist
-"D:\Programs\NSIS\makensis.exe" PiVTDesktop.nsi
+makensis.exe PiVTDesktop.nsi
 rmdir build /Q /S
 del *.pyc /Q
